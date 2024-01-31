99c311278fc8b3f7631717437bcbc12748a22c98
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2024-01-31 19:33:52 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,21 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 export default function Home() {
   return (
     <div className="w-full">
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Kelvin!</h2>
+        <p className="capitalize">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+
       <div className="mt-10 flex flex-col items-center justify-center text-zinc-400">
         <p> Paramos em 1h18m11s</p>
         <a href="https://youtu.be/aUkKMKJib0A?t=4691">
