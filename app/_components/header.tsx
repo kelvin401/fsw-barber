4c4dad0156184e5cdb00a96b8179aa4b60c86423
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2024-02-02 21:42:48 -0300

    feat: add oauth provider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,17 @@
+"use client";
 import Image from "next/image";
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data } = useSession();
+
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+
   return (
     <Card>
       <CardContent className="flex items-center justify-between px-5 py-8">
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,12 +2,12 @@ import { db } from "@/app/_lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import { Adapter } from "next-auth/adapters";
-import GoogleProveider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
-    GoogleProveider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_SECRET_ID as string,
     }),
