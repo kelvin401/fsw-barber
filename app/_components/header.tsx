cd572615d57f919a87d512214645861ac8abb100
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2024-02-04 20:51:32 -0300

    feat: ensure booking can be canceled

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,23 +1,19 @@
-"use client";
-import Image from "next/image";
-import {
-  CalendarIcon,
-  HomeIcon,
-  LogInIcon,
-  LogOutIcon,
-  MenuIcon,
-  UserIcon,
-} from "lucide-react";
-import { Button } from "./ui/button";
-import { Card, CardContent } from "./ui/card";
-import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
-import SideMenu from "./side-menu";
+'use client';
+import Image from 'next/image';
+import { MenuIcon } from 'lucide-react';
+import { Button } from './ui/button';
+import { Card, CardContent } from './ui/card';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import SideMenu from './side-menu';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="flex items-center justify-between px-5 py-8">
-        <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
+        <Link href="/">
+          <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
+        </Link>
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon" className="h-8 w-8">
--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -35,9 +35,11 @@ const BookingPage = async () => {
       <div className="px-5 py-6">
         <h1 className="text-xl font-bold">Agendamentos</h1>
 
-        <h2 className="mb-3 mt-4 text-sm font-bold uppercase text-gray-400">
-          Confirmados
-        </h2>
+        {confirmedBookings.length === 0 && finishedBookings.length === 0 && (
+          <h2 className="mb-3 mt-4 text-sm font-bold uppercase text-gray-400">
+            Confirmados
+          </h2>
+        )}
 
         <div className="flex flex-col gap-3">
           {confirmedBookings.map((booking) => (
